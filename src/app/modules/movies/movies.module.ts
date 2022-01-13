3aedfa8309d80cfdb3763dc82f640e366e22d6b2
nv1ruz <braian.martz@gmail.com>
2022-01-13 10:01:25 -0300

    import shared module

--- src/app/modules/movies/movies.module.ts
+++ src/app/modules/movies/movies.module.ts
@@ -1,19 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { SharedModule } from '../shared/shared.module';
 import { MoviesRoutingModule } from './movies-routing.module';
+
 import { MoviesListComponent } from './pages/movies-list/movies-list.component';
 import { MovieDetailComponent } from './pages/movie-detail/movie-detail.component';
 
-
 @NgModule({
-  declarations: [
-    MoviesListComponent,
-    MovieDetailComponent
-  ],
-  imports: [
-    CommonModule,
-    MoviesRoutingModule
-  ]
+    declarations: [MoviesListComponent, MovieDetailComponent],
+    imports: [CommonModule, MoviesRoutingModule, SharedModule],
 })
-export class MoviesModule { }
+export class MoviesModule {}
