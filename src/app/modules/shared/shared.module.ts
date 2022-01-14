be83f91b4441998b774947a9c34019f189d9cc94
nv1ruz <braian.martz@gmail.com>
2022-01-14 08:44:22 -0300

    add alert component

--- src/app/modules/shared/shared.module.ts
+++ src/app/modules/shared/shared.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TabsComponent } from './tabs/tabs.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { AlertComponent } from './alert/alert.component';
 
 @NgModule({
-    declarations: [TabsComponent, NavbarComponent],
+    declarations: [TabsComponent, NavbarComponent, AlertComponent],
     imports: [CommonModule],
-    exports: [TabsComponent, NavbarComponent],
+    exports: [TabsComponent, NavbarComponent, AlertComponent],
 })
 export class SharedModule {}
